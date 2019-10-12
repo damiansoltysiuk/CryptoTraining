0a1531db84fdd3b1b7f5bf7edcab1d3ccd4a4038
Damian Sołtysiuk <damian.soltysiuk@gmail.com>
2019-10-12 14:16:04 +0200

    added decrypt method

--- src/main/java/eu/szestkam/cryptotraining/cesar/CesarCipher.java
+++ src/main/java/eu/szestkam/cryptotraining/cesar/CesarCipher.java
@@ -2,8 +2,9 @@ package eu.szestkam.cryptotraining.cesar;
 
 public class CesarCipher {
     private String phrase;
+    private String ALPHABETH = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    CesarCipher(String text) {  
+    CesarCipher(String text) {
         this.phrase = text;
     }
 
@@ -11,8 +12,66 @@ public class CesarCipher {
         phrase = "";
     }
 
-    String encode() {
-        return phrase;
+    public static void main(String[] args) {
+        System.out.println(new CesarCipher("dOm").encode());
     }
 
+    String encode() {
+        //StringBuilder sb = new StringBuilder();
+        String result = "";
+        for(char ch : phrase.toCharArray()) {
+            int index;
+            if(ch >= 65 && ch <= 90) index= (ch + 3) % 64;
+            else if(ch >= 97 && ch <= 122) index = ((ch + 3) % 96);
+            else if(ch >= 48 && ch <= 57) index = ((ch + 3) % 47);
+            else index = ch;
+            char _ch = (char)index;
+
+            //sb.append(_ch + "");
+            result += String.valueOf((char)index);
+        }
+        return result;
+    }
+
+    String encodeKS() {
+        String code = "";
+        char[] letters = phrase.toCharArray();
+        for(char letter : letters) {
+            boolean isLower = isLower(letter);
+            Character _char = Character.toUpperCase(letter);
+            int val = charIndex(_char);
+            int newVal = (val + 3) % 26;
+            String newLetter = codeLetter(newVal);
+            code += (isLower) ? newLetter.toLowerCase() : newLetter.toUpperCase();
+
+        }
+        return code;
+    }
+
+    String decrypt() {
+        String decode = "";
+        char[] letters = phrase.toCharArray();
+        for(char letter : letters) {
+            boolean isLower = isLower(letter);
+            Character _char = Character.toUpperCase(letter);
+            int val = charIndex(_char);
+            int newVal = (val + 23) % 26;
+            String newLetter = codeLetter(newVal);
+            decode += (isLower) ? newLetter.toLowerCase() : newLetter.toUpperCase();
+
+        }
+        return decode;
+    }
+
+    private String codeLetter(int newVal) {
+        return String.valueOf(ALPHABETH.charAt(newVal));
+    }
+
+    private int charIndex(Character _char) {
+        return ALPHABETH.indexOf(_char);
+    }
+
+    private boolean isLower(char letter) {
+        return Character.isLowerCase(letter);
+    }
 }
--- src/test/java/eu/szestkam/cryptotraining/cesar/CesarCipherTest.java
+++ src/test/java/eu/szestkam/cryptotraining/cesar/CesarCipherTest.java
@@ -50,4 +50,76 @@ public class CesarCipherTest {
         assertEquals(SAMPLE_PHRASE.length(), encoded.length());
     }
 
+    @Test
+    public void testSimpleEncodeLowerCase() {
+        CesarCipher cipher = new CesarCipher("abc");
+        String code = "def";
+        String encode = cipher.encodeKS();
+        assertEquals(code, encode);
+
+        cipher = new CesarCipher("xyz");
+        code = "abc";
+        encode = cipher.encodeKS();
+        assertEquals(code, encode);
+
+    }
+
+    @Test
+    public void testSimpleEncodeMixedCase() {
+        CesarCipher cipher = new CesarCipher("abC");
+        String code = "deF";
+        String encode = cipher.encode();
+        assertEquals(code, encode);
+
+        cipher = new CesarCipher("xyZ");
+        code = "abC";
+        encode = cipher.encode();
+        assertEquals(code, encode);
+    }
+
+    @Test
+    public void testDecode() {
+        CesarCipher cipher = new CesarCipher("deF");
+        String code = "abC";
+        String encoded = cipher.decrypt();
+        assertEquals(code, encoded);
+    }
+
+//    @Test
+//    public void onlySmallLetterTest() {
+//        assertEquals("gdpldq", new CesarCipher("damian").decode());
+//        assertEquals("grp", new CesarCipher("dom").decode());
+//        assertEquals("nrw", new CesarCipher("kot").decode());
+//    }
+
+//    @Test
+//    public void onlyBigletterTest() {
+//        assertEquals("GDPLDQ", CesarCipher.decode("DAMIAN"));
+//        assertEquals("GRP", CesarCipher.decode("DOM"));
+//        assertEquals("NRW", CesarCipher.decode("KOT"));
+//    }
+//
+//    @Test
+//    public void letterTest() {
+//        assertEquals("abcDEF", CesarCipher.decode("xyzABC"));
+//        assertEquals("abc DEF", CesarCipher.decode("xyz ABC"));
+//    }
+//
+//    @Test
+//    public void digitsTest() {
+//        assertEquals("2134", CesarCipher.decode("9801"));
+//        assertEquals("22 66", CesarCipher.decode("99 33"));
+//    }
+//
+//    @Test
+//    void specialSymbolsTest() {
+//        assertEquals("....", CesarCipher.decode("...."));
+//        assertEquals("!@#$", CesarCipher.decode("!@#$"));
+//    }
+//
+//    @Test
+//    void fullTest() {
+//        assertEquals("DEFghi456!@#$", CesarCipher.decode("ABCdef123!@#$"));
+//    }
+
 }
